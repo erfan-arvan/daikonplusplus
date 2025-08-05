99494990cca826e05d900c65298707c9d5a1c2c4
Erfan Arvan <ea442@njit.edu>
2025-08-05 15:17:14 -0400

    fix/update-test-to-junit5 2

--- src/test/java/edu/njit/jerse/daikonplusplus/AppTest.java
+++ src/test/java/edu/njit/jerse/daikonplusplus/AppTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
     @Test public void appHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+	assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+
     }
 }
